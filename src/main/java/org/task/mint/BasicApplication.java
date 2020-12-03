ec98914315e4b46515a4d3472d43a55dcd7a3018
Christian  Oliver <obiekrix@gmail.com>
2020-12-03 01:25:36 +0100

    Added config classes for hibernate session factory

--- src/main/java/org/task/mint/BasicApplication.java
+++ src/main/java/org/task/mint/BasicApplication.java
@@ -1,9 +1,12 @@
 package org.task.mint;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
 
 @SpringBootApplication
+@EnableAutoConfiguration(exclude = HibernateJpaAutoConfiguration.class)
 public class BasicApplication {
 
     public static void main(String[] args) {
